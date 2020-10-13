774230df4df52eb97c4370c1a95afeb07fef891b
Renato <rntdek@gmail.com>
2020-10-12 22:17:17 -0300

    criacao tabela page home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,43 @@
 <!-- after login -->
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">
+                    <a href="#">Cadastrar</a>
+                </div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Altura</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">14</th>
+                                <td>Tyler</td>
+                                <td>1.96</td>
+                                <td>Action</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">9</th>
+                                <td>Rondo</td>
+                                <td>1.85</td>
+                                <td>Action</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">22</th>
+                                <td>Butler</td>
+                                <td>2.01</td>
+                                <td>Action</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
